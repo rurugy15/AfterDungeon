a5d3b844003d7751b7f2110ef203aec7f4e0d998
EQwon <bluetwintail0906@gmail.com>
2019-09-12 22:14:31 +0900

    Make basic movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float runSpeed = 40f;
+
+    private float horizontalMove = 0f;
+    private bool jump = false;
+
+    private void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump")) jump = true;
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, jump);
+        jump = false;
+    }
+}
