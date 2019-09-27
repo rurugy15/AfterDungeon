0872eec2af8152fac2263ea22bf1e87044327548
EQwon <bluetwintail0906@gmail.com>
2019-09-28 00:55:46 +0900

    몬스터 원거리 공격 간단하게 추가

---
+++ Assets/Scripts/MonsterAI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AttackType { Always, Detected };
+
+public class MonsterAI : MonoBehaviour
+{
+    [SerializeField] private AttackType attackType;
+
+    private CharacterShootController shooter;
+    private PatrolAreaController patroller;
+
+    private void Start()
+    {
+        patroller = GetComponent<PatrolAreaController>();
+
+        if (GetComponent<CharacterShootController>())
+            shooter = GetComponent<CharacterShootController>();
+        else enabled = false;
+    }
+
+    private void Update()
+    {
+        shooter.Shoot(transform.position, patroller.isFacingRight);
+    }
+}
--- Assets/Scripts/PatrolAreaController.cs
+++ Assets/Scripts/PatrolAreaController.cs
@@ -6,6 +6,7 @@ public class PatrolAreaController : MonoBehaviour
 {
     public List<Vector2> patrolPoints;
     public float speed;
+    public bool isFacingRight;
 
     private int targetPoint;
 
@@ -13,15 +14,20 @@ public class PatrolAreaController : MonoBehaviour
     {
         transform.position = patrolPoints[0];
         targetPoint = 1;
+        isFacingRight = true;
     }
 
     private void FixedUpdate()
     {
         MoveToNextPoint();
+        FlipCharacter();
     }
 
     private void MoveToNextPoint()
     {
+        if (transform.position.x < patrolPoints[targetPoint].x) isFacingRight = true;
+        if (transform.position.x > patrolPoints[targetPoint].x) isFacingRight = false;
+
         transform.position = Vector2.MoveTowards(transform.position, patrolPoints[targetPoint], speed * Time.fixedDeltaTime);
 
         if ((patrolPoints[targetPoint] - (Vector2)transform.position).magnitude < Mathf.Epsilon)
@@ -29,5 +35,12 @@ public class PatrolAreaController : MonoBehaviour
             targetPoint += 1;
             if (targetPoint == patrolPoints.Count) targetPoint = 0;
         }
+    }
+
+    private void FlipCharacter()
+    {
+        if (isFacingRight == true)
+            GetComponent<SpriteRenderer>().flipX = false;
+        else GetComponent<SpriteRenderer>().flipX = true;
     }
 }
