bc52257da8304d154a5dee55bfa35d8354aa7d51
EQwon <bluetwintail0906@gmail.com>
2019-12-23 22:16:38 +0900

    스테이지 클리어 포탈 제작 및 적용

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -10,8 +10,14 @@ public class Portal : MonoBehaviour
     {
         if (coll.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
-            coll.gameObject.transform.position = Exit.position;
-            coll.gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            GameObject player = coll.gameObject;
+            PortalAction(player);
         }
+    }
+
+    protected virtual void PortalAction(GameObject player)
+    {
+        player.transform.position = Exit.position;
+        player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
     }
 }
