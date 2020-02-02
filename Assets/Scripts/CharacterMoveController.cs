8192790823406c3a036560b1c13cdf7046b82082
EQwon <bluetwintail0906@gmail.com>
2020-02-02 22:06:37 +0900

    플레이어 점프 높이 3칸으로 수정 및 더블 점프 추가

--- Assets/Scripts/CharacterMoveController.cs
+++ Assets/Scripts/CharacterMoveController.cs
@@ -11,16 +11,10 @@ public class CharacterMoveController : MonoBehaviour
     [Header("Horizontal Movement")]
     [SerializeField] private LayerMask m_WhatIsGround;              // A mask determining what is ground to the character
     [SerializeField] private Transform groundChecker;               // A position marking where to check if the player is grounded.
-    [SerializeField] private float jumpVelocity;
-    [SerializeField] private float jumpingGravity;
-    [SerializeField] private float fallingGravity;
-    [SerializeField] private float maxInputTime;
-    [SerializeField] private float currentGravity;
-    private float jumpTimeCounter;
+    [SerializeField] private float jumpHeight;
+    private float jumpVelocity;
     private bool canJump = false;
-
-    [SerializeField] private float allowedJumpTime;                 // 플랫폼을 벗어난 뒤에도 점프를 할 수 있도록 허용된 시간
-    private float lastGroundedTime;
+    private bool canDoubleJump = false;
 
     const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
     private bool m_Grounded;            // Whether or not the player is grounded.
@@ -36,6 +30,7 @@ public class CharacterMoveController : MonoBehaviour
 
     private void Start()
     {
+        jumpVelocity = Mathf.Sqrt(2 * rb.gravityScale * 9.81f * jumpHeight);
         terminalVelocity = 2 * jumpVelocity;
     }
 
@@ -47,7 +42,13 @@ public class CharacterMoveController : MonoBehaviour
     private void GroundChecking()
     {
         // 상승 중에는 점프 불가
-        if (rb.velocity.y >= 0.01f) return;
+        if (rb.velocity.y >= 0.01f)
+        {
+            m_Grounded = false;
+            return;
+        }
+
+        m_Grounded = false;
 
         Collider2D[] colls = Physics2D.OverlapBoxAll(groundChecker.position, new Vector2(0.7f, k_GroundedRadius), 0, m_WhatIsGround);
 
@@ -56,23 +57,13 @@ public class CharacterMoveController : MonoBehaviour
             if (colls[i].gameObject != gameObject)
             {
                 m_Grounded = true;
-                lastGroundedTime = Time.time;
                 return;
             }
-        }
-
-        // 마지막으로 땅에 있었던 시간에서 허용된 점프 시간보다 오래 지나면 땅에서 떨어진 것으로 판정
-        if (Time.time - lastGroundedTime > allowedJumpTime)
-        {
-            m_Grounded = false;
-            canJump = false;
-        }            
+        }    
     }
 
     public void Move(float speed, bool jump)
     {
-        if (!canJump && !jump && m_Grounded) canJump = true;
-
         HorizontalVelocityControl(speed);
         JumpMovement(jump);
 
@@ -101,33 +92,25 @@ public class CharacterMoveController : MonoBehaviour
 
     private void JumpMovement(bool jump)
     {
-        if (!jump) jumpTimeCounter = 0;
+        if (!jump) canJump = true;
 
-        // If the player is on ground and try to jump
-        // 점프 상승 시작
+        // 땅을 밟을 시 초기화
+        if (m_Grounded)
+        {
+            canDoubleJump = true;
+        }
+
         if (m_Grounded && jump && canJump)
         {
-            jumpTimeCounter = maxInputTime;
-
-            canJump = false;
-            m_Grounded = false;
             rb.velocity = new Vector2(rb.velocity.x, jumpVelocity);
-            currentGravity = jumpingGravity;
+            canJump = false;
         }
-
-        // If the player is not ground and try to jump
-        // 점프 상승 도중
-        if (!m_Grounded && jump && jumpTimeCounter > 0)
+        if (!m_Grounded && jump && canJump && canDoubleJump)
         {
-            jumpTimeCounter -= Time.fixedDeltaTime;
-            currentGravity = jumpingGravity;
+            rb.velocity = new Vector2(rb.velocity.x, jumpVelocity);
+            canJump = false;
+            canDoubleJump = false;
         }
-
-        // Change gravity
-        if (!jump || jumpTimeCounter <= 0 || rb.velocity.y < 0)
-            currentGravity = fallingGravity;
-
-        rb.gravityScale = currentGravity;
     }
 
     private void HorizontalVelocityControl(float targetV)
