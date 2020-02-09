81c67e6e207cdb0e19f2266d16e16f7a60ec9ef2
EQwon <bluetwintail0906@gmail.com>
2020-02-09 21:08:05 +0900

    레버 플랫폼이 deactivate 됐을 시 화살이 떨어지도록 구현

---
+++ Assets/Scripts/ContactArrow/LeverPlatform.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverPlatform : LodgingPlatform
+{
+    public void Activate()
+    {
+        GetComponent<SpriteRenderer>().color = Color.red;
+        GetComponent<Collider2D>().enabled = true;
+    }
+
+    public void Deactivate()
+    {
+        GetComponent<SpriteRenderer>().color = new Color(1, 0, 0, 0.5f);
+        GetComponent<Collider2D>().enabled = false;
+    }
+}
