c0f1908834e3033c61f0b162babc8c1ef246f852
EQwon <bluetwintail0906@gmail.com>
2020-02-09 22:39:10 +0900

    레버가 작동했을 때 플랫폼 위에 플레이어가 있으면 작동을 미루도록 설정

--- Assets/Scripts/ContactArrow/LeverPlatform.cs
+++ Assets/Scripts/ContactArrow/LeverPlatform.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class LeverPlatform : LodgingPlatform
 {
+    [SerializeField] private LayerMask playerLayer;
+    private bool targetState = true;
+
     public void Activate()
     {
-        GetComponent<SpriteRenderer>().color = Color.red;
-        GetComponent<Collider2D>().enabled = true;
+        targetState = true;
+        StartCoroutine(TryToActivate());
     }
 
     public void Deactivate()
     {
+        targetState = false;
         GetComponent<SpriteRenderer>().color = new Color(1, 0, 0, 0.5f);
         GetComponent<Collider2D>().enabled = false;
+    }
+
+    private IEnumerator TryToActivate()
+    {
+        BoxCollider2D myColl = GetComponent<BoxCollider2D>();
+
+        Collider2D coll = Physics2D.OverlapBox(transform.localPosition, myColl.size, 0, playerLayer);
+
+        while (coll != null)
+        {
+            if (!targetState) yield break;
+
+            yield return new WaitForFixedUpdate();
+            coll = Physics2D.OverlapBox(transform.localPosition, myColl.size, 0, playerLayer);
+        }
+
+        GetComponent<SpriteRenderer>().color = Color.red;
+        GetComponent<Collider2D>().enabled = true;
     }
 }
