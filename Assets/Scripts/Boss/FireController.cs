cf3a26d77d816d3e2060e75546815f00f2b569c0
EQwon <bluetwintail0906@gmail.com>
2020-03-01 20:04:08 +0900

    dotween 추가

---
+++ Assets/Scripts/Boss/FireController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireController : MonoBehaviour
+{
+    [SerializeField] private Vector2 dir;
+    [SerializeField] private float speed;
+
+    public void Initialize(Vector2 dir, float speed)
+    {
+        this.dir = dir;
+        this.speed = speed;
+    }
+
+    private void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = dir * speed;
+        Debug.Log("Start");
+    }
+}
